be03b8dcfa5457b558db77343d517b3a910f74fd
Renaldo Hyacinthe <renaldohyacinthe@nyu.edu>
2026-06-12 20:59:37 -0400

    Revert "mobile video bugfix"
    This reverts commit b7c4844c3bc6da7da211248a5dd75e9fd2a46982.

--- script.js
+++ script.js
@@ -321,24 +321,14 @@ function appendMessage(text, type) {
 async function startCamera() {
     if (localStream) localStream.getTracks().forEach(t => t.stop());
 
-    const isMobile = window.innerWidth <= 768;
-
-const constraints = {
-    audio: true,
-    video: { 
-        facingMode: currentFacingMode,
-        ...(isMobile ? {} : { width: { ideal: 1280 }, height: { ideal: 720 } })
-    }
-};
+    const constraints = {
+        audio: true,
+        video: { facingMode: currentFacingMode, width: { ideal: 1280 }, height: { ideal: 720 } }
+    };
 
     try {
         localStream = await navigator.mediaDevices.getUserMedia(constraints);
         localVideo.srcObject = localStream;
-
-localVideo.play().catch(error => {
-    console.warn("Autoplay prevented by mobile browser:", error);
-});
-        
         localStream.getAudioTracks()[0].enabled = isAudioEnabled;
         localStream.getVideoTracks()[0].enabled = isVideoEnabled;
 
